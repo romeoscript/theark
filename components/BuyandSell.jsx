89e680de3e878077b2243a7d20ce8e4ab90636f1
romeoscript <romeobourne211@gmail.com>
2024-03-29 16:12:32 +0100

    address infor

--- components/BuyandSell.jsx
+++ components/BuyandSell.jsx
@@ -1,8 +1,7 @@
 "use client"
 import React, { useEffect, useState } from 'react'
-import { Table } from 'antd'
-import { MdVerified } from "react-icons/md";
-import { RiErrorWarningFill } from "react-icons/ri";
+import { Table, Tag } from 'antd'
+import eth from '../public/assets/eth.svg'
 
 const data = {
     "buys": [
@@ -457,32 +456,55 @@ const data = {
 const BuyandSell = () => {
     const columns = [
         {
-          title: 'Token Symbol & Name',
-          dataIndex: 'token_symbol',
-          key: 'token_symbol',
-          render: (text, record) => (
-            <div className='flex items-center justify-center gap-3'>
-              <img src={record.token_logo} alt={text} className='w-[30px] h-[30px] flex  rounded-full' />
-              {text} 
-            </div>
-          )
+            title: '',
+            dataIndex: 'token_symbol',
+            key: 'token_symbol',
+            render: (text, record) => (
+                <div>
+                    {record.token_logo ? <img src={record.token_logo} alt={text} className='w-10 h-10 rounded-full' /> : <div className='w-10 h-10 bg-gray-200 rounded-full'></div>}
+                </div>
+            )
         },
         {
-          title: 'Token Decimals',
-          dataIndex: 'token_decimals',
-          key: 'token_decimals',
+            title: 'Token  Name',
+            dataIndex: 'token_symbol',
+            key: 'token_symbol',
+            render: (text, record) => (
+                <div className='flex items-center justify-center gap-3'>
+                    {text}
+                </div>
+            )
         },
         {
-          title: 'From Address',
-          dataIndex: 'from_address',
-          key: 'from_address',
+            title: 'Token Decimals',
+            dataIndex: 'token_decimals',
+            key: 'token_decimals',
         },
         {
-          title: 'To Address',
-          dataIndex: 'to_address',
-          key: 'to_address',
+            title: 'Value',
+            dataIndex: 'value',
+            key: 'value',
+            render: text => parseFloat(text).toLocaleString()
         },
-      ];
+        {
+            title: 'Verified Contract',
+            dataIndex: 'verified_contract',
+            key: 'verified_contract',
+            render: (text, record) => (
+                <div className='flex items-center justify-center gap-3'>
+                    {record.verified_contract ? <Tag color="green">Verified</Tag> : <Tag color="red">Not Verified</Tag>}
+                </div>
+            )
+        },
+        {
+            title: 'Possible Spam',
+            dataIndex: 'possible_spam_time',
+            key: 'possible_spam_time',
+            render: (text, record) => (
+                record.possible_spam ? new Date(text).toLocaleString() : 'No'
+            )
+        },
+    ];
     return (
         <div>
             <h1>DataTable</h1>
@@ -492,14 +514,14 @@ const BuyandSell = () => {
                     dataSource={data.buys}
                     columns={columns}
 
-                    rowKey={(record, index) => index} 
+                    rowKey={(record, index) => index}
                 />
                 <h2>Sells</h2>
                 <Table
                     dataSource={data.sells}
                     columns={columns}
 
-                    rowKey={(record, index) => index} 
+                    rowKey={(record, index) => index}
                 />
             </div>
         </div>
